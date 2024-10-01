556cc80b43c851e7bb5f983529fa1815f13c9aa5
Luca <luca.renz@hotmail.com>
2024-10-01 22:38:47 +0200

    Revert "Add brand to article and output"
    This reverts commit ffa23dc6caf239dc1b229512ccc9fb44490a366f.

--- src/model/article.py
+++ src/model/article.py
@@ -2,11 +2,10 @@
 ##  Most general model that will be used for each individual type of article
 ##
 class Article:
-    def __init__(self, name, price, description, category, rating, brand, source):
+    def __init__(self, name, price, description, category, rating, source):
         self.name = name
         self.price = price
         self.description = description
         self.category = category
         self.rating = rating
-        self.brand = brand
-        self.source = source
+        self.source= source
